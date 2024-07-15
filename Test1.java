ff976fd299ddc4a02436569581f3ba232eed9d7b
yogisupra <yogisuprasmg@gmail.com>
2024-07-15 11:15:10 +0700

    Commit Pertama

---
+++ Test1.java
@@ -0,0 +1,5 @@
+public class Test1 {
+	public static void main(){
+	  System.out.println("Hello");
+	}
+}
---
+++ Test2.java
@@ -0,0 +1,12 @@
+public class Test2 {
+	public static void main(){
+	  int a;
+	  int b;
+	  int c;
+	  a=5;
+	  b=10
+	  c=a*b;
+	  System.out.println("hasil :"+c);
+	  
+	}
+}
